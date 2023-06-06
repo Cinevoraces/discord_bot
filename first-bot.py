ffb57d16775f18393a7db90f34c9e2b26c28376a
Julien Goletto <julien.goletto@gmail.com>
2023-06-06 11:26:38 +0200

    :rocket: Finished the sessionbot

--- first-bot.py
+++ first-bot.py
@@ -2,7 +2,7 @@ import os, datetime
 from pytz import timezone
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dataclasses import dataclass
 
 load_dotenv()
@@ -39,19 +39,44 @@ async def sum(ctx, *arr):
 class Session:
     is_active: bool = False
     start_time: int = 0
+    MAX_TIME: int = 60*60 # 1 hours
 
 session = Session()
 
+@tasks.loop(seconds=60, count=2)
+async def break_reminder():
+    # Mandatory for timecheck to run it twice since it will be stopped right after we start the session if we don't.
+    if break_reminder.current_loop == 0:
+        return
+    
+    channel = bot.get_channel(int(CHANNEL_ID))
+    await channel.send(f"**You should take a break !** You've been working for {session.MAX_TIME / 60} minutes !")
+
+def date_converter(date):
+    return date.replace(tzinfo=datetime.timezone.utc).astimezone(timezone("Europe/Paris")).strftime("%H:%M:%S")
+def human_readable_time(duration):
+    return str(datetime.timedelta(minutes=duration))
+
 @bot.command()
 async def start_session(ctx):
-  if session.is_active:
-    await ctx.send("Session already started")
-  else:
-      session.is_active = True
-      session.start_time = ctx.message.created_at
-      converted_date : datetime = session.start_time.replace(tzinfo=datetime.timezone.utc).astimezone(timezone("Europe/Paris")).strftime("%H:%M:%S")
-      await ctx.send("Session started at " + converted_date)
+    if session.is_active:
+        await ctx.send("Session already started")
+    else:
+        session.is_active = True
+        break_reminder.start()
+        session.start_time = ctx.message.created_at
+        await ctx.send("Session started at " + date_converter(session.start_time))
 
+@bot.command()
+async def end_session(ctx):
+    if not session.is_active:
+        await ctx.send("No current session")
+    else:
+        session.is_active = False
+        session.end_time = ctx.message.created_at
+        duration = datetime.timedelta(minutes=(session.end_time - session.start_time).seconds/60)
+        break_reminder.stop()
+        await ctx.send("Session ended at " + date_converter(session.end_time) + " after " + str(duration))
 
 bot.run(BOT_TOKEN)
 
