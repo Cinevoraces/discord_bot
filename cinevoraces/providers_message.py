349b7242d2e6fa43d0e41fa5e53642df7ff5daa9
Julien Goletto <julien.goletto@gmail.com>
2023-06-13 17:50:41 +0200

    :bug: Fixed previously introduced bug

--- cinevoraces/providers_message.py
+++ cinevoraces/providers_message.py
@@ -9,7 +9,7 @@ def set_message_content(movie_title, region, availability):
     # Extraction of different providers options and flatten it keeping only names
     flatrate, buy, rent = (availability[k] if k in availability else [] for k in ("flatrate", "buy", "rent"))
     flatrate_providers_name, buy_providers_name, rent_providers_name = (
-        list(map(lambda provider: provider['provider_name'], option if len(option) != 0 else [])) for option in options
+        list(map(lambda provider: provider['provider_name'], option if len(option) != 0 else [])) for option in [flatrate, buy, rent]
     )
 
     message_header = f"Voici les disponibilités de **{movie_title}** pour la région {region} :\n\n"
