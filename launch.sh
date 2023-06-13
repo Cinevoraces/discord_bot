4b055e592a607ab6631e00f3212af3753f7fd425
Julien Goletto <julien.goletto@gmail.com>
2023-06-13 11:18:10 +0200

    :bug: Parrallel scripts and logs

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
-python ~/discord_bots/recover_movie.py &
-python ~/discord_bots/get_availability.py &
-python ~/discord_bots/guess_the_movie.py &
+nohup python ~/discord_bots/recover_movie.py > recover_movie.log 2>&1  &
+nohup python ~/discord_bots/get_availability.py > get_availability.log 2>&1  &
+nohup python ~/discord_bots/guess_the_movie.py > guess_the_movie.log 2>&1  &
